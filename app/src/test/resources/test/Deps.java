1f83c8bb5786b1f88b1fb6a16b65d80f29a19902
nsanson <nico.sanson@gmail.com>
2021-06-28 17:15:14 +0200

    Ajout resources de test

---
+++ app/src/test/resources/test/Deps.java
@@ -0,0 +1,11 @@
+package test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Deps {
+	public static void main(String[] args) {
+		System.out.println(Math.random());
+		Set<String> set = new HashSet<>();
+	}
+}
